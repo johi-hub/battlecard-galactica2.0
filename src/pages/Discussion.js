cf19a58481e93662c02f400407592f1c76135596
johanna <jd.34736.jdlt@gmail.com>
2021-09-06 16:21:44 -0400

    added about, discussion files to pages folder

---
+++ src/pages/Discussion.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Discussion = () => {
+    return (
+        <div>
+            <Header />
+            discussion
+        </div>
+    )
+}
+
+export default Discussion
