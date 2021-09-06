596e7f4868f3222543b7172969cd27c52b168f67
johanna <jd.34736.jdlt@gmail.com>
2021-09-06 16:43:32 -0400

    added components to hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.section``;
+
+
 
 const Hero = () => {
     return (
-        <>
-            hero
-        </>
+        <Section>
+            <Container>
+                <h1>title</h1>
+                <p>description</p>
+                <button>learn more</button>
+            </Container>
+        </Section>
     )
 }
 
